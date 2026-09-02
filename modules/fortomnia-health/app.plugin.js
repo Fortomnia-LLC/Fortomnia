2c5240198e90206fc8fc58545eeabda1ac782e09
grc0830-source <grc0830@gmail.com>
2026-09-02 16:04:41 -0400

    Add HealthKit background delivery: update modules/fortomnia-health/app.plugin.js

--- modules/fortomnia-health/app.plugin.js
+++ modules/fortomnia-health/app.plugin.js
@@ -1,6 +1,10 @@
 const { withEntitlementsPlist, withInfoPlist } = require("expo/config-plugins");
 module.exports = function withFortomniaHealth(config) {
-  config = withEntitlementsPlist(config, (config) => { config.modResults["com.apple.developer.healthkit"] = true; return config; });
+  config = withEntitlementsPlist(config, (config) => {
+    config.modResults["com.apple.developer.healthkit"] = true;
+    config.modResults["com.apple.developer.healthkit.background-delivery"] = true;
+    return config;
+  });
   config = withInfoPlist(config, (config) => {
     config.modResults.NSHealthShareUsageDescription = "Fortomnia reads health and activity data you choose to share to personalize training, recovery, and progress insights.";
     config.modResults.NSHealthUpdateUsageDescription = "Fortomnia can save workouts and body measurements to Apple Health when you choose to enable it.";
@@ -8,3 +12,4 @@ module.exports = function withFortomniaHealth(config) {
   });
   return config;
 };
+
